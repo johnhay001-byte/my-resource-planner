0331bedd2488fc381d26216cbf33e36866a4b029
John Hay <johnhay001@gmail.com>
2025-10-15 18:57:14 +0100

    feat: Add team assignment to projects

--- src/firebase.js
+++ src/firebase.js
@@ -1,34 +1,20 @@
-    import { initializeApp } from "firebase/app";
-    import { getFirestore } from "firebase/firestore";
-
-    // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD3looppMOn2k_waQp4EuDC5WH9KfNUPfM",
   authDomain: "my-resource-planner.firebaseapp.com",
   projectId: "my-resource-planner",
-  storageBucket: "my-resource-planner.firebasestorage.app",
+  storageBucket: "my-resource-planner.appspot.com",
   messagingSenderId: "765125484407",
   appId: "1:765125484407:web:53708796b14d6ef5900b72"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-    const firebaseConfig = {
-      apiKey: "AIza...",
-      authDomain: "your-project.firebaseapp.com",
-      projectId: "your-project-id",
-      storageBucket: "your-project.appspot.com",
-      messagingSenderId: "...",
-      appId: "..."
-    };
-    // ---------------------------------------------
 
-    const app = initializeApp(firebaseConfig);
-    export const db = getFirestore(app);
-    
+// Export the Firestore database instance for the rest of the app to use
+export const db = getFirestore(app);
 
