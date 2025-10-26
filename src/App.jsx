f29baadc05a485320760b4f948695f40869795bf
John Hay <johnhay001@gmail.com>
2025-10-26 22:14:22 +0000

    Forcing redeploy with stable files

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// Forcing a new build - 1
 import React, { useState, useEffect, useMemo } from 'react';
 import { db } from './firebase'; 
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
